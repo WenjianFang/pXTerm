a19360240312f42854854cde89e403b86efcb4dd
wenjian <fwjse1992@gmail.com>
2018-01-28 23:30:18 +0800

    implement create new session and connect to exist session GUI

---
+++ pXTermCONFIG.py
@@ -0,0 +1,35 @@
+
+class pXTermCONFIG:
+    def __init__(self, configFile = 'sessions'):
+        self.configs = configFile
+
+    def dump_configs(self):
+        with open(self.configs, 'r') as sessionFile:
+            ss = sessionFile.readlines()
+            for i in range(0, len(ss)):
+                ss[i] = ss[i].strip('\n')
+
+        return ss
+
+    def write_config(self, session):
+        sessionString = ''
+        with open(self.configs, 'ab+') as sessionFile:
+            for i in range(0, len(session)):
+                if i != (len(session) - 1):
+                    sessionString = sessionString + session[i] + ' '
+                else:
+                    sessionString = sessionString + session[i] + '\n'
+            sessionFile.write(sessionString)
+
+    def get_config_by_session_name(self, sessionName):
+        ss = self.dump_configs()
+        for session in ss:
+            if session.split(' ')[0] == sessionName:
+                return session.split(' ')
+
+    def get_all_sessions_name(self):
+        sessionsName = []
+        ss = self.dump_configs()
+        for session in ss:
+            sessionsName.append(session.split(' ')[0])
+        return sessionsName
--- pXTermGUI.py
+++ pXTermGUI.py
@@ -5,6 +5,16 @@ FangWenjian, fwjse1992@gmail.com
 """
 
 import wx
+import sys
+import logging
+
+from pXTermCONFIG import pXTermCONFIG
+
+logger = logging.getLogger('pXTermGUI')
+logger.addHandler(logging.StreamHandler(sys.stdout))
+logger.setLevel(logging.INFO)
+
+configs = pXTermCONFIG('sessions')
 
 class pXTermFrame(wx.Frame):
     """
@@ -15,72 +25,57 @@ class pXTermFrame(wx.Frame):
         # ensure the parent's __init__ is called
         super(pXTermFrame, self).__init__(*args, **kw)
 
-        # create a panel in the frame
-        pnl = wx.Panel(self)
-
         # create a menu bar
-        self.makeMenuBar()
-
-        # and a status bar
-        self.CreateStatusBar()
-        self.SetStatusText("Welcome to pXTerm version 1.0")
-
-
-    def makeMenuBar(self):
-        """
-        A menu bar is composed of menus, which are composed of menu items.
-        This method builds a set of menus and binds handlers to be called
-        when the menu item is selected.
-        """
-
         # Make a session menu with new session and exist session items
-        sessionMenu = wx.Menu()
+        self.sessionMenu = wx.Menu()
         # The "\t..." syntax defines an accelerator key that also triggers
         # the same event
-        newSessionItem = sessionMenu.Append(-1, "&New Session...\tCtrl-N",
-                "Create new session")
-        sessionMenu.AppendSeparator()
+        self.newSessionItem = self.sessionMenu.Append(wx.ID_ANY, "&New Session")
 
-        connectExistSessionItem = sessionMenu.Append(-1, "&Connect Session...\tCtrl-C",
-                "Connect exist new session")
-        sessionMenu.AppendSeparator()
+        self.connectExistSessionItem = self.sessionMenu.Append(wx.ID_ANY, "&Connect Session")
 
         # When using a stock ID we don't need to specify the menu item's
         # label
-        exitItem = sessionMenu.Append(wx.ID_EXIT)
+        self.exitItem = self.sessionMenu.Append(wx.ID_EXIT)
 
         # Now a help menu for the about item
-        helpMenu = wx.Menu()
-        aboutItem = helpMenu.Append(wx.ID_ABOUT)
+        self.helpMenu = wx.Menu()
+        self.aboutItem = self.helpMenu.Append(wx.ID_ABOUT)
 
         # Make the menu bar and add the two menus to it. The '&' defines
         # that the next letter is the "mnemonic" for the menu item. On the
         # platforms that support it those letters are underlined and can be
         # triggered from the keyboard.
-        menuBar = wx.MenuBar()
-        menuBar.Append(sessionMenu, "&Session")
-        menuBar.Append(helpMenu, "&Help")
+        self.menuBar = wx.MenuBar()
+        self.menuBar.Append(self.sessionMenu, "&Session")
+        self.menuBar.Append(self.helpMenu, "&Help")
 
         # Give the menu bar to the frame
-        self.SetMenuBar(menuBar)
+        self.SetMenuBar(self.menuBar)
 
         # Finally, associate a handler function with the EVT_MENU event for
         # each of the menu items. That means that when that menu item is
         # activated then the associated handler function will be called.
-        self.Bind(wx.EVT_MENU, self.OnNewSession, newSessionItem)
-        self.Bind(wx.EVT_MENU, self.OnConnectSession, connectExistSessionItem)
-        self.Bind(wx.EVT_MENU, self.OnExit,  exitItem)
-        self.Bind(wx.EVT_MENU, self.OnAbout, aboutItem)
+        self.Bind(wx.EVT_MENU, self.OnNewSession, self.newSessionItem)
+        self.Bind(wx.EVT_MENU, self.OnConnectExistSession, self.connectExistSessionItem)
+        self.Bind(wx.EVT_MENU, self.OnExit, self.exitItem)
+        self.Bind(wx.EVT_MENU, self.OnAbout, self.aboutItem)
 
+        # and a status bar
+        self.CreateStatusBar()
+        self.SetStatusText("Welcome to pXTerm version 1.0")
 
     def OnExit(self, event):
         """Close the frame, terminating the application."""
         self.Close(True)
 
 
-    def OnConnectSession(self, event):
-        """connect exist session."""
-        wx.MessageBox("This is connect session")
+    def OnConnectExistSession(self, event):
+        existSessionFrame = wx.Frame(self, title='Connect to Exist Session')
+
+        ExistSessionPanel(existSessionFrame)
+
+        existSessionFrame.Show(True)
         # new window and shows exist session with item,
         # then click the item to connect related session
 
@@ -90,10 +85,12 @@ class pXTermFrame(wx.Frame):
         """create new session and then connect to this session."""
         #wx.MessageBox("This is new session")
         newSessionFrame = wx.Frame(self, title="Create New Session")
+        NewSessionPanel(newSessionFrame)
+
         newSessionFrame.Show(True)
         #new window to create a new session
-        # then connec to this session
 
+        # then connec to this session
 
     def OnAbout(self, event):
         """Display an About Dialog"""
@@ -102,6 +99,108 @@ class pXTermFrame(wx.Frame):
                       "About pXTerm",
                       wx.OK|wx.ICON_INFORMATION)
 
+class NewSessionPanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+
+        # create some sizers
+        mainSizer = wx.BoxSizer(wx.VERTICAL)
+        grid = wx.GridBagSizer(hgap=5, vgap=5)
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.quote = wx.StaticText(self, label="Create New Session: ")
+        grid.Add(self.quote, pos=(0,0))
+
+        # A button
+        self.button = wx.Button(self, label="Save")
+        self.Bind(wx.EVT_BUTTON, self.clickSaveButton, self.button)
+        self.Bind(wx.EVT_COMMAND_ENTER, self.clickSaveButton, self.button)
+
+        # the edit control - session name.
+        self.sessionNameText = wx.StaticText(self, label="Session name :")
+        grid.Add(self.sessionNameText, pos=(1,0))
+        self.editSessionName = wx.TextCtrl(self, size=(140,-1))
+        grid.Add(self.editSessionName, pos=(1,1))
+
+        # the edit control - session ip.
+        self.sessionIPText = wx.StaticText(self, label="IP addr :")
+        grid.Add(self.sessionIPText, pos=(2,0))
+        self.editSessionIP = wx.TextCtrl(self, size=(140,-1))
+        grid.Add(self.editSessionIP, pos=(2,1))
+
+        # the combobox Control
+        self.portList = ['22']
+        self.portText = wx.StaticText(self, label="port")
+        grid.Add(self.portText, pos=(3,0))
+        self.portBox = wx.ComboBox(self, value='22', size=(95, -1), choices=self.portList, style=wx.CB_DROPDOWN)
+        grid.Add(self.portBox, pos=(3,1))
+
+        # the edit control - login account.
+        self.loginText = wx.StaticText(self, label="Login account :")
+        grid.Add(self.loginText, pos=(4,0))
+        self.loginAccount = wx.TextCtrl(self, size=(140,-1))
+        grid.Add(self.loginAccount, pos=(4,1))
+
+
+        # the edit control - login password.
+        self.passwordText = wx.StaticText(self, label="Login password :")
+        grid.Add(self.passwordText, pos=(5,0))
+        self.password = wx.TextCtrl(self, size=(140,-1), style = wx.TE_PASSWORD)
+        grid.Add(self.password, pos=(5,1))
+
+
+        hSizer.Add(grid, 0, wx.ALL, 5)
+
+        mainSizer.Add(hSizer, 0, wx.ALL, 5)
+        mainSizer.Add(self.button, 0, wx.CENTER)
+        self.SetSizerAndFit(mainSizer)
+
+    def clickSaveButton(self, event):
+        sn = self.editSessionName.GetValue()
+        sip = self.editSessionIP.GetValue()
+        sport = self.portBox.GetValue()
+        slogin = self.loginAccount.GetValue()
+        spass = self.password.GetValue()
+        logger.info("session name: " + sn)
+        logger.info("session ip: " + sip)
+        logger.info("session port: " + sport)
+        logger.info("session login account: " + slogin)
+        logger.info("session login password: " + spass)
+        session = [sn, sip, sport, slogin, spass]
+        configs.write_config(session)
+
+        wx.MessageBox("Save the new session", "New Session", wx.OK|wx.ICON_INFORMATION)
+
+class ExistSessionPanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+
+        # create some sizers
+        mainSizer = wx.BoxSizer(wx.VERTICAL)
+        grid = wx.GridBagSizer(hgap=5, vgap=5)
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.quote = wx.StaticText(self, label="Exist Sessions: ")
+        grid.Add(self.quote, pos=(0,0))
+
+        self.sessionsName = configs.get_all_sessions_name()
+        # the edit control - session name.
+        sessionList = []
+        for name in self.sessionsName:
+            session = configs.get_config_by_session_name(name)
+            sessionList.append((session[0] + '\t' + session[1]))
+
+        self.sessionListBox = wx.ListBox(self, wx.ID_ANY, choices=sessionList, style=wx.LB_SINGLE)
+        self.Bind(wx.EVT_LISTBOX_DCLICK, self.ConnectSession, self.sessionListBox)
+
+        grid.Add(self.sessionListBox, pos=(1,0))
+
+        hSizer.Add(grid, 0, wx.ALL, 5)
+        mainSizer.Add(hSizer, 0, wx.ALL, 5)
+        self.SetSizerAndFit(mainSizer)
+
+    def ConnectSession(self, event):
+        logger.info("session: " + event.GetEventObject().GetStringSelection())
 
 if __name__ == '__main__':
     # When this module is run (not imported) then create the app, the
